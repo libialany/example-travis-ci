a1088303a676aaf4f3e851cad8af3b9c3043d4fc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-07-17 10:31:50 +0000

    Deploy libialany/example-travis-ci to github.com/libialany/example-travis-ci.git:gh-pages

---
+++ build/precache-manifest.1be65422db820ee072a64d50bb9dc0b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2010ad4874d9d1477a8229d511e339",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1739ba9b262b3dfa66cf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2ea6dc12c6b7bb4452fa",
+    "url": "/static/js/2.a73051b3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.a73051b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1739ba9b262b3dfa66cf",
+    "url": "/static/js/main.1fbdca48.chunk.js"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
